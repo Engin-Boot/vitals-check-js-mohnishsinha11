100d3409485aabdac702d321ddb321015e9409ee
Mohnish <mohnishsinha011@gmail.com>
2020-08-30 16:23:28 +0530

    reduce the cyclomatic complexity

--- checker.js
+++ checker.js
@@ -3,13 +3,9 @@ const checkBpmReference=require("./checkBpm.js");
 const checkSpo2Reference=require("./checkSpo2.js");
 const checkRespRateReference=require("./checkRespRate.js");
 function vitalsAreOk(bpm, spo2, respRate) {
-    if(checkBpmReference(bpm)) {
+    if(checkBpmReference(bpm) || checkSpo2Reference(spo2) || checkRespRateReference(respRate)) {
         return false;
-    } else if(checkSpo2Reference(spo2)) {
-        return false;
-    } else if(checkRespRateReference(respRate)) {
-        return false;
-    }
+    } 
     console.log("All body vitals are okay");
     return true;
 }
@@ -23,5 +19,4 @@ expect(vitalsAreOk(100, 101, 70)).to.be.false;
 expect(vitalsAreOk(100, 95, 100)).to.be.false;
 expect(vitalsAreOk(70, 90, 30)).to.be.true;
 expect(vitalsAreOk(150, 100, 95)).to.be.true;
-
 console.log('checker is done');
